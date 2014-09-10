3f9fe3b195186bd982e084f53b385e5a718cee83
Shane Osbourne <shane.osbourne8@gmail.com>
2014-09-10 20:14:21 +0100

    User parser generator

--- example.js
+++ example.js
@@ -1,14 +1,14 @@
 var compile  = require("./index");
 var chalk    = require("chalk");
-var compiler = require("./index").Compiler({
-    prefix: "%Cblue:[%R%Cmagenta:tFunk%R%Ccyan:]%R ",
-    custom: {
-        "warn": "chalk.bgRed.white",
-        "shane": function (string) {
-            return string + "Oh ho";
-        }
-    }
-});
+//var compiler = require("./index").Compiler({
+//    prefix: "%Cblue:[%R%Cmagenta:tFunk%R%Ccyan:]%R ",
+//    custom: {
+//        "warn": "chalk.bgRed.white",
+//        "shane": function (string) {
+//            return string + "Oh ho";
+//        }
+//    }
+//});
 
 console.log( compile("%Cred:This has a single colour") );
 
@@ -20,28 +20,30 @@ console.log( compile("%Cgreen:This has %Ccyan:two colours") );
 
 console.log( compile("%Cgreen:This has a colour %Ccyan:nested inside%R another colour") );
 
-console.log( compile("This has a custom %Cwarn:'warn'%R style", {
-    "warn": "chalk.bgRed.white"
-}));
+console.log( compile("%Cgreen:This has %Ccyan:THREE levels of %Cmagenta:nested%R colors%R that can be reset") );
 
-console.log( compiler.compile("This has a cool %Cblue:Prefix") );
-
-console.log( compiler.compile("%Cblue:Prefix %Ccyan:with NESTED%R styles and a custom %Cwarn:'warn'") );
-
-console.log( chalk.blue("Multiple " + chalk.cyan("NESTED") + " styles in " + chalk.red("the same string") + " with an ending"));
-
-console.log( compile("%Cblue:Multiple %Ccyan:NESTED%R styles in %Cred:the same string%R with an ending") );
-
-
-console.log( chalk.green("This has a colour " + chalk.cyan("nested inside") + " another colour") );
-
-//tFunk
-console.log( compile("%Cgreen:This has a colour %Ccyan:nested inside%R another colour") );
-
-compiler = require("./index").Compiler({
-    prefix: "[%Cmagenta:tFunk%R]"
-});
-
-console.log( compiler.compile(" tFunk is awesome") );
-console.log( compiler.compile(" don't you think?") );
+//console.log( compile("This has a custom %Cwarn:'warn'%R style", {
+//    "warn": "chalk.bgRed.white"
+//}));
+//
+//console.log( compiler.compile("This has a cool %Cblue:Prefix") );
+//
+//console.log( compiler.compile("%Cblue:Prefix %Ccyan:with NESTED%R styles and a custom %Cwarn:'warn'") );
+//
+//console.log( chalk.blue("Multiple " + chalk.cyan("NESTED") + " styles in " + chalk.red("the same string") + " with an ending"));
+//
+//console.log( compile("%Cblue:Multiple %Ccyan:NESTED%R styles in %Cred:the same string%R with an ending") );
+//
+//
+//console.log( chalk.green("This has a colour " + chalk.cyan("nested inside") + " another colour") );
+//
+////tFunk
+//console.log( compile("%Cgreen:This has a colour %Ccyan:nested inside%R another colour") );
+//
+//compiler = require("./index").Compiler({
+//    prefix: "[%Cmagenta:tFunk%R]"
+//});
+//
+//console.log( compiler.compile(" tFunk is awesome") );
+//console.log( compiler.compile(" don't you think?") );
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var chalk = require("chalk");
+var chalk  = require("chalk");
+var parser = require("./lib/parser");
 
 /**
  * Stateless compiler.
@@ -8,13 +9,12 @@ var chalk = require("chalk");
  * @param {Object} [custom] - Any custom methods
  * @returns {String}
  */
-
 var chalkError = "'%s' not supported. See https://github.com/sindresorhus/chalk#styles for supported styles.";
 
 function compile(string, custom) {
     var res;
     try {
-        res = addColors(removeDupes(fixNestedIndexes(fixEnding(string))), custom);
+        res = parseAst(createAst(parser, string));
     } catch (e) {
         var color = /Property '(.+?)'/.exec(e.message);
 
@@ -28,311 +28,81 @@ function compile(string, custom) {
 }
 
 /**
- * @param {Object} opts
- * @returns {Compiler}
- */
-function Compiler(opts) {
-
-    opts = opts || {};
-
-    if (opts.prefix) {
-        this.prefix = compile(opts.prefix, opts.custom);
-    }
-
-    this.compile = function (string) {
-
-        return this.prefix + compile(string, opts.custom);
-
-    }.bind(this);
-
-    return this;
-}
-
-/**
- * Get the indexes of matches
- * @param {String} string
- * @returns {{starts: Array, ends: Array}}
- */
-function getIndexes(string) {
-
-    var starts = [];
-    var ends = [];
-
-    string.replace(/%C/g, function () {
-        starts.push(arguments[1]);
-    });
-
-    string.replace(/%R/g, function () {
-        ends.push(arguments[1]);
-    });
-
-    return {
-        starts: starts,
-        ends: ends
-    };
-}
-
-/**
- * Remove any sections that are NOT nested, but were caught in the match
- * @param {Array} starts
- * @param {Array} ends
- */
-function dropNoneNested(starts, ends) {
-
-    var newStarts = [starts[0]];
-    var newEnds = [ends[0]];
-
-    for (var i = 0, n = starts.length; i < n; i += 1) {
-        if (i) {
-            if (starts[i] < ends[i - 1]) {
-                newStarts.push(starts[i]);
-                newEnds.push(ends[i]);
-            }
-        }
-    }
-
-    return {
-        starts: newStarts,
-        ends: newEnds
-    };
-}
-
-/**
- * Add the missing colour starts
- * @param string
- */
-function fixNested(string) {
-
-    if (!isNested(string)) {
-        return string;
-    }
-
-    var indexes          = getIndexes(string);
-    var newIndexes       = dropNoneNested(indexes.starts, indexes.ends);
-    var sectionEnd       = newIndexes.ends[(newIndexes.ends.length - 1)] + 2;
-
-    var stringSubSection = string.slice(newIndexes.starts[0], sectionEnd);
-    var oldStart         = string.slice(0, newIndexes.starts[0]);
-    var oldEnd           = string.slice(sectionEnd);
-
-    var colors = [];
-
-    stringSubSection.replace(/%C([a-z]+?):/g, function () {
-        colors.push(arguments[1]);
-    });
-
-    var count = 0;
-
-    var replaced = stringSubSection.replace(/%R/g, function () {
-        var string = "%R%C" + colors[count] + ":";
-        count += 1;
-        return string;
-    });
-
-    return oldStart + replaced + oldEnd;
-}
-
-/**
- * Check if there's a nested group
- * @param {String} string
- * @returns {Boolean}
- */
-function isNested(string) {
-
-    var indexes = getIndexes(string);
-    var starts = indexes.starts;
-    var ends = indexes.ends;
-
-    var nested = false;
-
-    for (var i = 0, n = starts.length; i < n; i += 1) {
-
-        var nextStart;
-        var nextEnd;
-        var currentEnd = ends[i];
-
-        if (i !== (starts.length - 1)) {
-
-            nextStart = starts[i + 1];
-            nextEnd = ends[i + 1];
-
-            if (nextStart < currentEnd) {
-                nested = true;
-                return true;
-            }
-        }
-    }
-
-    return nested;
-}
-
-/**
- * String splice
- * @param {String} str
- * @param {Number} index
- * @param {String} add
- * @returns {string}
- */
-function spliceSlice(str, index, add) {
-    var before = str.slice(0, index);
-    var after = str.slice(index);
-    return [before, add, after].join("");
-}
-
-/**
- * Specifically for use with NESTED items
+ * @param parser
  * @param string
  * @returns {*}
  */
-function fixNestedIndexes(string) {
-
-    if (!isNested(string)) {
-
-        return string;
-
-    } else {
-
-        var indexes = getIndexes(string);
-        var sectionEnd = indexes.ends[(indexes.ends.length - 1)] + 2;
-        var stringSubSection = string.slice(indexes.starts[0], sectionEnd);
-
-        var context = "";
-        var old = "";
-        var initial = "";
-
-        if (indexes.starts[0] > 0) {
-            initial = string.slice(0, indexes.starts[0]);
-        }
-
-        return initial + stringSubSection.replace(/^(%C([a-zA-Z]+?):)([\s\S]+?)%R(?!.*%R)/g, function () {
-            context = arguments[2];
-            old = arguments[0];
-            var content = arguments[3];
-            return  trim(content, context, arguments[1]);
-        });
-    }
+function createAst(parser, string) {
+    return parser.parse(string);
 }
 
 /**
- * @param content
- * @param context
- * @param start
- * @returns {string}
+ * @param ast
  */
-function trim(content, context, start) {
+function parseAst(ast) {
 
-    var indexes = getIndexes(content);
+    var color  = null;
+    var colors = [];
 
-    if (indexes.ends[indexes.ends.length - 1] < content.length) {
+    return ast.reduce(function (joined, item) {
 
-        // Match that needs fixing
-        var add = "%C" + context + ":";
+        if (item.color && item.text) {
+            color = item.color;
+            colors.push(item.color);
+            return joined + chalk[item.color](item.text);
+        }
 
-        var fixed;
-        var count = 0;
+        if (item.buffer) {
 
-        indexes.ends.forEach(function (item, i) {
-            if (!fixed) {
-                fixed = spliceSlice(content, indexes.ends[i] + 2 + count, add);
+            if (item.buffer === "%R") {
+                return joined;
+            }
+            if (colors.length) {
+                return joined + chalk[colors[colors.length-1]](item.buffer);
             } else {
-                fixed = spliceSlice(fixed, indexes.ends[i] + 2 + count, add);
+                return joined + item.buffer;
             }
-            count += add.length;
-        });
+        }
 
-        return start + (fixed || "") + "%R";
+        if (item.reset) {
 
-    } else {
+            colors.pop();
 
-        return start + content + "%R";
-    }
-}
-
-/**
- *
- * @param string
- * @returns {Array|{index: number, input: string}|*}
- */
-function isValidChalkMethod(string) {
-    return string.match(/^([a-zA-Z\.]+)$/);
-}
-
-/**
- * Run each match through chalk
- * @param {String} string
- * @param {Object} [custom]
- * @returns {String}
- */
-function addColors(string, custom) {
-
-    var regex = /%C([a-zA-Z]+?):([\s\S]+?)(?=%C|%R)/g;
-
-    var newstring = string.replace(regex, function () {
-
-        var color = arguments[1];
-        var func;
-
-        if (custom && custom[color]) {
-
-            if (typeof custom[color] === "string") {
-
-                if (isValidChalkMethod(custom[color])) {
-                    func = eval(custom[color]);
+            if (item.text) {
+                if (colors.length) {
+                    return joined + chalk[colors[colors.length-1]](item.text);
                 } else {
-                    throw Error(chalkError.replace("%s", custom[color]));
+                    return joined + item.text;
                 }
-            } else if (typeof custom[color] === "function") {
-                func = custom[color];
             }
-
-            return func(arguments[2]);
         }
 
-        return chalk[color](arguments[2]);
+        return joined;
 
-    });
-
-    return newstring.replace(/%R/g, "");
+    }, "");
 }
 
-/**
- * Remove any instances with no content %Cred:%R
- * @param string
- * @returns {string}
- */
-function removeDupes(string) {
-    return string.replace(/%C([a-z]+?):%R/g, "");
-}
-
-/**
- * Ensure all colours are closed
- * @param {String} string
- * @returns {String}
- */
-function fixEnding(string) {
-
-    var indexes = getIndexes(string);
-
-    var diff = indexes.starts.length - indexes.ends.length;
-
-    var append = "";
-
-    if (diff > 0) {
-        for (var i = 0, n = diff; i < n; i += 1) {
-            append += "%R";
-        }
-        return string + append;
-    }
-
-    return string;
-}
+///**
+// * @param {Object} opts
+// * @returns {Compiler}
+// */
+//function Compiler(opts) {
+//
+//    opts = opts || {};
+//
+//    if (opts.prefix) {
+//        this.prefix = compile(opts.prefix, opts.custom);
+//    }
+//
+//    this.compile = function (string) {
+//
+//        return this.prefix + compile(string, opts.custom);
+//
+//    }.bind(this);
+//
+//    return this;
+//}
 
 module.exports = compile;
-module.exports.isNested = isNested;
-module.exports.fixNested = fixNested;
-module.exports.dropNoneNested = dropNoneNested;
-module.exports.getIndexes = getIndexes;
-module.exports.fixNestedIndexes = fixNestedIndexes;
-module.exports.addColors = addColors;
-module.exports.fixEnding = fixEnding;
-module.exports.Compiler = Compiler;
+//module.exports.fixEnding = fixEnding;
+//module.exports.Compiler = Compiler;
